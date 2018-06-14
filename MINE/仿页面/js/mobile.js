99c993be58c8e13aae590805b651ebf103189cb0
Mr-zsy <921358361@qq.com>
2018-06-14 16:07:08 +0800

    last

--- MINE/仿页面/js/mobile.js
+++ MINE/仿页面/js/mobile.js
@@ -1,13 +1,16 @@
 $(function() {
+	$(window).resize(function() {
+		menu();
+	});
 	if ($(window).innerWidth() <= 1023) {
 		//二维码显示
 		$(".wei:nth-of-type(1)").click(function() {
-			if($("#erweima").css("top") == "-210px"){
+			if ($("#erweima").css("top") == "-210px") {
 				$("#erweima").css({
-				visibility: 'visible',
-				top: '-250px'
-			});
-			}else{
+					visibility: 'visible',
+					top: '-250px'
+				});
+			} else {
 				$("#erweima").css({
 					visibility: 'hidden',
 					top: '-210px'
@@ -18,26 +21,9 @@ $(function() {
 		//导航栏宽度改变
 		$(".blank").height($(".nav-wrapper").height());
 		//菜单栏切换
-		$("#m-nav-wrapper").height($(window).innerHeight() - 80);
-		$(".lines").click(function() {
-			if ($(".second-line").width() != 0) {
-				console.log("点了");
-				$(".second-line").width(0);
-				$(".second-line").height(0);
-				$(".first-line").addClass('rotate45');
-				$(".third-line").addClass('rotate-45');
-				$("#m-nav-wrapper").css("margin-top", "0");
-				$("body").css("overflow", "hidden");
-			} else {
-				$(".second-line").width(24);
-				$(".second-line").height(3);
-				$(".first-line").removeClass('rotate45');
-				$(".third-line").removeClass('rotate-45');
-				$("#m-nav-wrapper").css("margin-top", "-9999px");
-				$("body").css("overflow", "auto");
-			}
-		});
+		menu();
 	}
+
 	if ($(window).innerWidth() <= 1023 && $(window).innerWidth() > 414) {
 		//替换第八张图
 		$(".article-item:nth-of-type(8) img:nth-of-type(1)").attr({
@@ -84,3 +70,25 @@ $(function() {
 		});
 	}
 })
+
+//菜单栏切换函数
+function menu() {
+	$("#m-nav-wrapper").height($(window).innerHeight() - 80);
+	$(".lines").click(function() {
+		if ($(".second-line").width() != 0) {
+			$(".second-line").width(0);
+			$(".second-line").height(0);
+			$(".first-line").addClass('rotate45');
+			$(".third-line").addClass('rotate-45');
+			$("#m-nav-wrapper").css("margin-top", "0");
+			$("body").css("overflow", "hidden");
+		} else {
+			$(".second-line").width(24);
+			$(".second-line").height(3);
+			$(".first-line").removeClass('rotate45');
+			$(".third-line").removeClass('rotate-45');
+			$("#m-nav-wrapper").css("margin-top", "-9999px");
+			$("body").css("overflow", "auto");
+		}
+	});
+}
